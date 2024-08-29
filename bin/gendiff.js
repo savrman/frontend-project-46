82ce53d23916e05095a3b126e73252b781531ae6
Savr Mandzhiev <163935933+savrman@users.noreply.github.com>
2024-08-29 22:31:49 +0300

    Add description and version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,9 @@ import { Command } from 'commander';
 
 const program = new Command();
 
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1');
+
 program.parse();
