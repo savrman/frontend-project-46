23f044c4c5a0d07d6c1351afe14aec55d6795429
Savr Mandzhiev <163935933+savrman@users.noreply.github.com>
2024-09-19 00:30:36 +0300

    Change gendiff output from text to tree

--- src/gendiff.js
+++ src/gendiff.js
@@ -7,23 +7,53 @@ const genDiff = (data1, data2) => {
 
     const diff = keys.reduce((arr, item) => {
       if (!Object.hasOwn(data1, item)) {
-        arr[item] = 'added';
+        arr.push({
+          name: item,
+          type: 'flat',
+          state: 'added',
+          currentValue: data2[item],
+        });
       } else if (!Object.hasOwn(data2, item)) {
-        arr[item] = 'deleted';
+        arr.push({
+          name: item,
+          type: 'flat',
+          state: 'deleted',
+          oldValue: data1[item],
+        });
       } else if (data1[item] !== data2[item]) {
         if (typeof data1[item] === 'object' && typeof data2[item] === 'object') {
-          arr[item] = iter(data1[item], data2[item]);
+          arr.push({
+            name: item,
+            type: 'nested',
+            children: iter(data1[item], data2[item]),
+          });
         } else {
-          arr[item] = 'changed';
+          arr.push({
+            name: item,
+            type: 'flat',
+            state: 'changed',
+            oldValue: data1[item],
+            currentValue: data2[item],
+          });
         }
       } else {
-        arr[item] = 'unchanged';
+        arr.push({
+          name: item,
+          type: 'flat',
+          state: 'unchanged',
+          currentValue: data2[item],
+        });
       }
       return arr;
-    }, {});
+    }, []);
     return diff;
   }
-  return iter(data1, data2);
+  return {
+    type: 'root',
+    children: [
+      ...iter(data1, data2)
+    ],
+  }
 };
 
 export default genDiff;
