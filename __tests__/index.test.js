dc86a05458bf51207d9b16040984313129261283
Savr Mandzhiev <163935933+savrman@users.noreply.github.com>
2024-10-16 11:31:36 +0300

    Add fixtures and tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,29 @@
+import path, { dirname } from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (...paths) => path.join(__dirname, '..', '__fixtures__', ...paths);
+
+const deepTypes = ['flat', 'nested'];
+const fileExtensions = ['json', 'yml'];
+
+describe.each(deepTypes)('Deep %s', (deepType) => {
+  describe.each(fileExtensions)('File extension %s', (fileExtension) => {
+    test.each(['plain', 'stylish'])('Format %s', (format) => {
+      const fixturePath1 = getFixturePath(deepType, `file1.${fileExtension}`);
+      const fixturePath2 = getFixturePath(deepType, `file2.${fileExtension}`);
+      const expected = fs.readFileSync(getFixturePath(deepType, `expected-file1-file2-${format}`), 'utf-8');
+      expect(genDiff(fixturePath1, fixturePath2, format)).toEqual(expected);
+    });
+    test.each(['json'])('Format %s', (format) => {
+      const fixturePath1 = getFixturePath(deepType, `file1.${fileExtension}`);
+      const fixturePath2 = getFixturePath(deepType, `file2.${fileExtension}`);
+      const expected = JSON.parse(fs.readFileSync(getFixturePath(deepType, `expected-file1-file2-${format}`), 'utf-8'));
+      expect(genDiff(fixturePath1, fixturePath2, format)).toEqual(expected);
+    });
+  });
+});
